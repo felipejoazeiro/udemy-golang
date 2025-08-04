21f2dd4f9800df96a3ab300c7d1c78f7cf04711c
felipe.joazeiro <felipe.joazeiro@ictcondor.com.br>
2025-08-04 15:27:08 -0300

    Generators

---
+++ 18-Concorrencia/6-PadroesDeConcorrencia/6.2-Generators/generator.go
@@ -0,0 +1,22 @@
+package main
+
+func main(){
+	canal := escrever("Olá mundo!")
+
+	for i := 0; i < 10; i++ {
+		fmt.Println(<-canal)
+	}
+}
+
+func escrever(texto string) <-chan string {
+	canal := make(chan string)
+
+	go func() {
+		for {
+			canal <- fmt.Sprintg("Valor recebido: %s", texto)
+			time.Sleep(time.Millisecond * 500)
+		}
+	}()
+
+	return canal
+}
